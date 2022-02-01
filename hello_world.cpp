31586fb5c66a185f5f7e2fc675aa33cbc1a1097d
Grigorii Lutkov <friend.lga@gmail.com>
2022-02-02 01:26:04 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!" << std::endl << "Григорий Лутков" << std::endl;
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Lutkov Grigorii" << std::endl;
+  std::cout << "Лутков Григорий" << std::endl;
   return 0;
 }
